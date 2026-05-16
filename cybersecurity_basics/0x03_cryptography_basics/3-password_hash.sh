6e35003abfb2ded1042aea8758c154db4d05a236
hafi <hafi@kali.hafi>
2026-05-16 18:03:22 -0400

    modified 3-password_hash.sh

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-salt=$(openssl rand -base64 12 | tr -dc 'a-zA-Z0-9' | head -c 16) echo -n "${salt}$1" | openssl dgst -sha512
+salt=$(openssl rand -base64 12 | tr -dc 'a-zA-Z0-9' | head -c 16) echo -n "${salt}$1" | openssl dgst -sha512 > 3_hash.txt
