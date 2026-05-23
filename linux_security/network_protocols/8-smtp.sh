67927dbd9261ecd02f1e27924eec7e94727fcdfc
hafi <hafi@kali.hafi>
2026-05-22 20:18:45 -0400

    modified with postfix 8-smtp.sh

--- linux_security/network_protocols/8-smtp.sh
+++ linux_security/network_protocols/8-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -i "^smtpd_tls_security_level" /etc/potfix/main.cf 2>/dev/null || echo "STARTTLS not configures"
+grep -i "^smtpd_tls_security_level" /etc/postfix/main.cf 2>/dev/null || echo "STARTTLS not configures"
