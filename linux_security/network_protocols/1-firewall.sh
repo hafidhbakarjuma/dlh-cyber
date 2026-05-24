7e23ab0e751a6435dd467ddc92ce194811476f39
hafi <hafi@kali.hafi>
2026-05-24 05:43:40 -0400

    modfied 1-firewall.sh

--- linux_security/network_protocols/1-firewall.sh
+++ linux_security/network_protocols/1-firewall.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 iptables -F
-iptables -P INPUT DROP && iptables -A INPUT -p tcp --dport ssh -j ACCEPT && iptables -L
+iptables -P INPUT DROP && iptables -A INPUT -p tcp --dport ssh -j ACCEPT && iptables -A INPUT -j DROP
