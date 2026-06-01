bde2e59b4f09071cad115b9568a3274f9a65e368
hafi <hafi@kali.hafi>
2026-06-01 06:52:45 -0400

    Add 5-port_scanner.py

---
+++ scripting_cyber/0x01-python_scripting/5-port_scanner.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module to check if a specific port is open on a host."""
+
+import socket
+
+def check_port(host, port):
+	"""Checks if a specific port is open on a host.
+
+	Args:
+		host (str): The hostname or IP address to check.
+		port (init): The port number to check.
+
+	Returns:
+		bool: True id the port is open, False if closed or unreachable.
+	"""
+	try:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.settimeout(3)
+		result = sock.cconnect_ex((host, port))
+		sock.close()
+		return result == 0
+	except Exception:
+		return False
