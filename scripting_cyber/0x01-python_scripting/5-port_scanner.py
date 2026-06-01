491154d9a5c966db0641238cc0d88f9fda4f6b7d
hafi <hafi@kali.hafi>
2026-06-01 07:03:12 -0400

    modified  5-port_scanner.py

--- scripting_cyber/0x01-python_scripting/5-port_scanner.py
+++ scripting_cyber/0x01-python_scripting/5-port_scanner.py
@@ -16,7 +16,7 @@ def check_port(host, port):
 	try:
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.settimeout(3)
-		result = sock.cconnect_ex((host, port))
+		result = sock.connect_ex((host, port))
 		sock.close()
 		return result == 0
 	except Exception:
