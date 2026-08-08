6f941dad087aaecb771c0423f263a08b4b356eba
Hafidh Bakari Juma <hafidhbakar504@gmail.com>
2026-08-08 14:50:59 +0200

    Refine error handling settings in auditd script

--- blue_team/2x02_eyes_on_endpoint/5-auditd_refine.sh
+++ blue_team/2x02_eyes_on_endpoint/5-auditd_refine.sh
@@ -8,7 +8,9 @@
 # Author: Hafidh Juma
 # Project: MedDefense Endpoint Telemetry Engineering
 
-set -euo pipefail
+set -e
+set -u
+set -o pipefail
 
 ##############################################################
 # Configuration
